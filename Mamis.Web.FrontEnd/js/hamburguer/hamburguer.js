152c28a8864eb8d93ff4b2f9dd6cf2d9cfff0ff5
luccve <hurryblank@gmail.com>
2022-06-16 17:40:31 -0300

    altertacoes

--- Mamis.Web.FrontEnd/js/hamburguer/hamburguer.js
+++ Mamis.Web.FrontEnd/js/hamburguer/hamburguer.js
@@ -1,10 +1,10 @@
 const btnMobile = document.getElementById('btn-mobile');
 
 
-
 function toggleMenu(event) {
   if (event.type === 'touchstart') event.preventDefault();
   const nav = document.getElementById('nav');
+  console.log(nav)
   nav.classList.toggle('active');
   const active = nav.classList.contains('active');
   event.currentTarget.setAttribute('aria-expanded', active);
