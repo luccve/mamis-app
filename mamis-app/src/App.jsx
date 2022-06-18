d17187e63158cd348174c9d331a8d78574dc5659
luccve <hurryblank@gmail.com>
2022-06-18 20:23:23 -0300

    Instalação da biblioteca react-youtube e 4 pag add

--- mamis-app/src/App.jsx
+++ mamis-app/src/App.jsx
@@ -2,6 +2,10 @@ import Header from "./components/header/header"
 import Footer from "./components/footer/footer"
 import Faq from "./pages/oquee/faq"
 import Historia from "./pages/historia/historia"
+import Mae from "./pages/maeparamae/maeparamae"
+import Quemsomos from "./pages/quemsomos/quemsomos"
+import Comoajudar from "./pages/comoajudar/comoajudar"
+import Missao from "./pages/missao/missao"
 import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
@@ -17,6 +21,18 @@ function App() {
           </Routes>
           <Routes>
             <Route path="/historia" element={<Historia />}></Route>
+          </Routes>
+          <Routes>
+            <Route path="/maeparamae" element={<Mae />}></Route>
+          </Routes>
+          <Routes>
+            <Route path="/quemsomos" element={<Quemsomos />}></Route>
+          </Routes>
+          <Routes>
+            <Route path="/missao" element={<Missao />}></Route>
+          </Routes>
+          <Routes>
+            <Route path="/comoajudar" element={<Comoajudar />}></Route>
           </Routes>
       </Router>
       <Footer />
